b5a26a3366a556ce1bd55960f6ba9793c2ed5060
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-10-05 01:29:21 +0200

    Gun Scriptable Object
    Now the object "Gun" can be created from the asset menu. The "Gun" script contains most of the necessary features to create a gun. This list of features can be updated as the needs for more complex weapons grow.

---
+++ ProtoContollerRogueLike/Assets/AK_Script/Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
+public class Gun : ScriptableObject
+{
+    public new string name;
+    public string ID;
+
+    // General Information
+    public float damage;
+    public float accuracy;
+    public int ammoCount;
+    public GameObject ammoType;
+
+
+    // Specific
+    public float dropRate;
+    public string rarity;
+
+    // Sound
+    public List<AudioSource> gunSounds = new List<AudioSource>();
+
+}
