3fb927d3b498283b3139e6e2faccda8387ca200e
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-10-06 18:06:51 +0200

    Zombie Rotation
    The script for the rotation was found in the Unity Forums http://answers.unity.com/answers/637243/view.html

--- ProtoContollerRogueLike/Assets/AK_Script/Enemy.cs
+++ ProtoContollerRogueLike/Assets/AK_Script/Enemy.cs
@@ -19,6 +19,14 @@ public class Enemy : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, playerPos.transform.position, speed * Time.deltaTime);
 
 
+
+            // Cette partie du script fut trouvee sur le forum Unity https://answers.unity.com/questions/585035/lookat-2d-equivalent-.html?_ga=2.230719519.1043224240.1601999147-1783980511.1597703941
+            Vector3 diff = playerPos.position - gameObject.transform.position;
+            diff.Normalize();
+
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
         }
 
     }
