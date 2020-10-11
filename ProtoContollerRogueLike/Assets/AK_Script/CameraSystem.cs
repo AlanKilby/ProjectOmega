1bb9e3090a0bcf40ce3d3e1b30743165b0d7d554
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-10-11 12:17:39 +0200

    Camera System Room

---
+++ ProtoContollerRogueLike/Assets/AK_Script/CameraSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    public RoomTriggerCollider isPlayerInRoom;
+    public Transform mainCameraPos;
+
+    private void Start()
+    {
+        isPlayerInRoom = GetComponentInParent<RoomTriggerCollider>();
+        mainCameraPos = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    private void Update()
+    {
+        if (isPlayerInRoom.playerIsInTheRoom)
+        {
+            mainCameraPos.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y,mainCameraPos.position.z);
+        }
+    }
+}
