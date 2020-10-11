133abacd009bd564de11ca9eb826659f9fe2028e
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-10-11 11:44:03 +0200

    Enemy Spawn

---
+++ ProtoContollerRogueLike/Assets/AK_Script/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform currentRoom;
+    public EnemyList enemyList;
+    public RoomTriggerCollider isPlayerInRoom;
+    public float spawnChance;
+    public float waitTime;
+
+    private void Start()
+    {
+        currentRoom = gameObject.transform.parent;
+        isPlayerInRoom = currentRoom.GetComponentInParent<RoomTriggerCollider>();
+        enemyList = GameObject.FindGameObjectWithTag("Enemy List").GetComponent<EnemyList>();
+        Invoke("SpawnEnemy", waitTime);
+    }
+
+    private void SpawnEnemy()
+    {
+        int i;
+
+        i = Random.Range(1, 101);
+
+        if (i <= spawnChance && i >= 1)
+        {
+            Debug.Log("Spawned");
+            Instantiate(enemyList.enemies[0], new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0), Quaternion.identity, currentRoom.transform);
+        }
+    }
+}
