40efb77b6795975fc8dd1fa0c69fd33295007ff9
Ulric <ulric.debaert@outlook.fr>
2020-10-11 15:26:34 +0200

    EscapePod

---
+++ ProtoContollerRogueLike/Assets/UD_Script/EscapePod.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapePod : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("can escape");
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+}
