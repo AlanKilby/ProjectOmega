17e48321f76201099b38ddb9dcdb6317feff5a64
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-11-16 18:03:23 +0100

    RoomDesigner

---
+++ ProtoContollerRogueLike/Assets/AK/AK_Script/RoomDesign.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDesign : MonoBehaviour
+{
+    public GameObject[] roomDesign;
+
+    int roomPicker;
+
+    private void Start()
+    {
+        roomPicker = Random.Range(0, roomDesign.Length);
+        Instantiate(roomDesign[roomPicker], new Vector2(transform.position.x,transform.position.y), Quaternion.identity, transform);
+    }
+
+}
