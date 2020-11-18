432a0fd6ba7845504a89b6b6b80ecf6f5fdcdfb5
Ulric <ulric.debaert@outlook.fr>
2020-11-18 16:57:56 +0100

    HealthBarWorking + New DashIcon

---
+++ ProtoContollerRogueLike/Assets/UD/UD_Script/UD_UI_Script/UI_HPBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HPBar : MonoBehaviour
+{
+    Image HealthBar;
+
+    PlayerHealth PH;
+
+    void Start()
+    {
+        PH = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        HealthBar = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        HealthBar.fillAmount = PH.healthPercent;
+    }
+}
