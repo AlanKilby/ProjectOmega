697202b95680cd8331e59986c976cbb057e23bf8
Ulric <ulric.debaert@outlook.fr>
2020-12-23 16:14:46 +0100

    TentaclesWorking + BossPhase2 WIP

---
+++ ProtoContollerRogueLike/Assets/UD/UD_Script/UD_Boss/UD_BossStageTwo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UD_BossStageTwo : MonoBehaviour
+{
+    [Header("Tentacle Manager")]
+    [SerializeField] GameObject tentacleObject;
+    [SerializeField] Transform[] tentacleSpawn;
+    /*[SerializeField] Transform tentacleSpawn1;
+    [SerializeField] Transform tentacleSpawn2;
+    [SerializeField] Transform tentacleSpawn3;
+    [SerializeField] Transform tentacleSpawn4;*/
+    [SerializeField] float spawnRate;
+    float spawnRateTimer;
+    [SerializeField] float tentacleLifeDelaySet;
+    bool canSpawn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (canSpawn)
+        {
+            for (int i = 0; i < tentacleSpawn.Length; i++)
+            {
+                GameObject tentacle = Instantiate(tentacleObject, tentacleSpawn[i].position, tentacleSpawn[i].rotation);
+                UD_TentacleBoss tentacleScript = tentacleSpawn[i].GetComponent<UD_TentacleBoss>();
+                tentacleScript.lifeTimer = tentacleLifeDelaySet;
+            }
+            /*GameObject tentacle1 = Instantiate(tentacleObject, tentacleSpawn1.position, tentacleSpawn1.rotation);
+            UD_TentacleBoss tentacle1Script = tentacleSpawn1.GetComponent<UD_TentacleBoss>();
+            tentacle1Script.lifeTimer = tentacleLifeDelaySet;
+            GameObject tentacle2 = Instantiate(tentacleObject, tentacleSpawn2.position, tentacleSpawn2.rotation);
+            GameObject tentacle3 = Instantiate(tentacleObject, tentacleSpawn3.position, tentacleSpawn3.rotation);
+            GameObject tentacle4 = Instantiate(tentacleObject, tentacleSpawn4.position, tentacleSpawn4.rotation);*/ //VERSION DEGUEUE DE CE QUI EST FAIT AU DESSUS
+            spawnRateTimer = spawnRate;
+            canSpawn = false;
+        }
+        AcideSprayTimer();
+    }
+
+    void AcideSprayTimer()
+    {
+        if (!canSpawn)
+        {
+            spawnRateTimer -= Time.deltaTime;
+            if (spawnRateTimer <= 0)
+            {
+                canSpawn = true;
+            }
+        }
+    }
+}
---
+++ ProtoContollerRogueLike/Assets/UD/UD_Script/UD_Boss/UD_TentacleBoss.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UD_TentacleBoss : MonoBehaviour
+{
+    [SerializeField] LayerMask whatIsPlayer;
+
+    Animator anim;
+    UD_BossBase boss;
+    Transform playerPos;
+
+    /*[HideInInspector]*/ public float lifeTimer;
+    [SerializeField] GameObject hitToPlayerDetector;
+    [SerializeField] float hitToPlayerDetectorDelay;
+    [SerializeField] float hitToPlayerDetectorDelayBeforeAttack;
+    [SerializeField] float ownAttackLaunchDetectionRadius;
+
+    [SerializeField] float attackRate;
+    float attackRateTimer;
+
+    [SerializeField] float health;
+    [SerializeField] int damageToBossOnDeath;
+
+    bool playerIsInAttackArea;
+    bool canAttack;
+    bool tentacleUp;
+    bool canTurn;
+
+    //Ajout Gus
+    private Material matWhite;
+    private Material matDefault;
+    SpriteRenderer sr;
+    //
+
+    void Start()
+    {
+        //boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<UD_BossBase>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        tentacleUp = true;
+        canTurn = true;
+    }
+
+    void Update()
+    {
+        if(canAttack && playerIsInAttackArea)
+        {
+            StartCoroutine(TentacleAttackShoot());
+        }
+        AttackReloadTimer();
+        LifeTimer();
+        if (canTurn)
+        {
+            PlayerDirection();
+        }
+        UpdateAnims();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckSurroundings();
+    }
+    IEnumerator TentacleAttackShoot()
+    {
+        canTurn = false;
+        yield return new WaitForSeconds(hitToPlayerDetectorDelayBeforeAttack);
+        tentacleUp = false;
+        attackRateTimer = attackRate;
+        canAttack = false;
+        hitToPlayerDetector.SetActive(true);
+        //Moment ou la tentacule claque à terre
+        yield return new WaitForSeconds(hitToPlayerDetectorDelay);
+        canTurn = true;
+        tentacleUp = true;
+        hitToPlayerDetector.SetActive(false);
+    }
+
+    private void CheckSurroundings()
+    {
+        playerIsInAttackArea = Physics2D.OverlapCircle(gameObject.transform.position, ownAttackLaunchDetectionRadius, whatIsPlayer);
+    }
+
+    void AttackReloadTimer()
+    {
+        if (!canAttack)
+        {
+            attackRateTimer -= Time.deltaTime;
+            if (attackRateTimer <= 0)
+            {
+                canAttack = true;
+            }
+        }
+    }
+
+    void LifeTimer()
+    {
+        lifeTimer -= Time.deltaTime;
+        if(lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void PlayerDirection()
+    {
+        Vector3 diff = playerPos.position - gameObject.transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        //Ajout Gus
+        sr.material = matWhite;
+        if (health <= 0)
+        {
+            Death();
+        }
+        else
+        {
+            Invoke("ResetMaterial", 0.1f);
+        }
+        //
+    }
+
+    public void Death()
+    {
+        boss.TakeDamage(damageToBossOnDeath);
+        Destroy(gameObject);
+    }
+
+    void UpdateAnims()
+    {
+        anim.SetBool("tentacleUp", tentacleUp);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(gameObject.transform.position, ownAttackLaunchDetectionRadius);
+    }
+
+    //Ajout Gus
+    void ResetMaterial()
+    {
+        sr.material = matDefault;
+    }
+    //
+}
