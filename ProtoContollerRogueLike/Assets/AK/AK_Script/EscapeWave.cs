5e3cd36074c16599e58ea697ef40366890922456
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-12-26 12:59:17 +0100

    Escape Waves

---
+++ ProtoContollerRogueLike/Assets/AK/AK_Script/EscapeWave.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeWave : MonoBehaviour
+{
+
+    public bool waveStarts = false;
+
+    public float timerBetweenWaves;
+    private float timerHolder;
+
+    public EnemyList enemyList;
+    public Transform currentRoom;
+
+    private void Start()
+    {
+        timerHolder = timerBetweenWaves;
+        currentRoom = gameObject.transform.parent;
+        enemyList = GameObject.FindGameObjectWithTag("Enemy List").GetComponent<EnemyList>();
+    }
+    void Update()
+    {
+        if (waveStarts)
+        {
+            timerBetweenWaves -= Time.deltaTime;
+        }
+
+        if(timerBetweenWaves <= 0)
+        {
+            SummonMonster();
+            timerBetweenWaves = timerHolder;
+        }
+    }
+
+
+    public void SummonMonster()
+    {
+        int j = Random.Range(0, enemyList.enemies.Length);
+        Instantiate(enemyList.enemies[j], new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0), Quaternion.identity, currentRoom.transform);
+    }
+}
--- ProtoContollerRogueLike/Assets/UD/UD_Script/UD_SceneManagement/EscapePod.cs
+++ ProtoContollerRogueLike/Assets/UD/UD_Script/UD_SceneManagement/EscapePod.cs
@@ -5,10 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class EscapePod : MonoBehaviour
 {
+    public float escapeTimer;
+
+    private bool escapeStart = false;
+
+    public GameObject[] escapeSpawners;
+
     private void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("can escape");
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            escapeStart = true;
+
+            for (int i = 0; i < escapeSpawners.Length; i++)
+            {
+                escapeSpawners[i].GetComponent<EscapeWave>().waveStarts = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (escapeStart)
+        {
+            escapeTimer -= Time.deltaTime;
+        }
+
+        if(escapeTimer <= 0)
         {
             SceneManager.LoadScene("UD_HUBtest");
         }
