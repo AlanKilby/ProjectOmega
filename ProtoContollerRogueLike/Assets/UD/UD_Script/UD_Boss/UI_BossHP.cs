21ccff735827ba8cb168235403b78acbe1b531d3
Ulric <ulric.debaert@outlook.fr>
2021-01-03 10:16:17 +0100

    BossHPbar

---
+++ ProtoContollerRogueLike/Assets/UD/UD_Script/UD_Boss/UI_BossHP.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_BossHP : MonoBehaviour
+{
+    Image hpBar;
+    UD_BossBase boss;
+
+    float hpBossMax;
+    float hpBossCurrent;
+
+    void Start()
+    {
+        hpBar = GetComponent<Image>();
+        boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<UD_BossBase>();
+        hpBossMax = boss.health;
+    }
+
+    void Update()
+    {
+        hpBossCurrent = boss.health;
+        hpBar.fillAmount = hpBossCurrent / hpBossMax;
+    }
+}
