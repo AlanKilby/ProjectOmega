a6773ccb7802eb694301d5329bc788600cb26538
Ulric <ulric.debaert@outlook.fr>
2021-01-04 19:10:55 +0100

    PlayerFreezeAfterMenu FIX

--- ProtoContollerRogueLike/Assets/HD_Script/MainMenu.cs
+++ ProtoContollerRogueLike/Assets/HD_Script/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        GameManagement.GameIsPaused = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         FindObjectOfType<AudioManager>().Play("Press Play");
         FindObjectOfType<AudioManager>().Play("Fight Music");
---
+++ ProtoContollerRogueLike/Assets/UD/UD_Script/GameIsPausedIndicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameIsPausedIndicator : MonoBehaviour
+{
+    public bool gameIsPausedIndiactor;
+
+    void Start()
+    {
+        gameIsPausedIndiactor = GameManagement.GameIsPaused;
+    }
+
+    void Update()
+    {
+        gameIsPausedIndiactor = GameManagement.GameIsPaused;
+    }
+}
