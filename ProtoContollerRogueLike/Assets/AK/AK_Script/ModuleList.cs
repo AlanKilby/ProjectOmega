a42308ab489f441525c953c35b74319d9d5bc150
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-01-10 12:32:40 +0100

    Hot Fix

--- ProtoContollerRogueLike/Assets/AK/AK_Script/ModuleList.cs
+++ ProtoContollerRogueLike/Assets/AK/AK_Script/ModuleList.cs
@@ -20,28 +20,36 @@ public class ModuleList : MonoBehaviour
     {
         // Il reste a faire un changement pour que le drop ne soit pas fait lorsque l'inventaire de modules est plein ! 
 
-        //randomLoot = Random.Range(0, modules.Length);
-        randomLoot = 1;
+        randomLoot = Random.Range(0, modules.Length);
+        //randomLoot = 1;
         Debug.Log(randomLoot);
 
         for(int i = 0; i <= player.GetComponent<Inventory>().moduleGameObject.Length; i++)
         {
-            Debug.Log("entered for loop");
+            Debug.Log(i);
 
-            if (player.GetComponent<Inventory>().moduleGameObject[i] != null && modules[randomLoot] == player.GetComponent<Inventory>().moduleGameObject[i])
-            {
-                randomLoot = Random.Range(0, modules.Length);
-                i = -1;
-                Debug.Log(randomLoot);
-                Debug.Log("reset loop");
-            }
-            
-            if(i >= player.GetComponent<Inventory>().moduleGameObject.Length)
+
+
+            if (i >= player.GetComponent<Inventory>().moduleGameObject.Length)
             {
                 GameObject drop = Instantiate(modules[randomLoot], thisObject.transform.position, Quaternion.identity);
                 drop.transform.SetParent(moduleParent);
             }
-            
+            if (i != player.GetComponent<Inventory>().moduleGameObject.Length && player.GetComponent<Inventory>().moduleGameObject[i] != null && modules[randomLoot] == player.GetComponent<Inventory>().moduleGameObject[i])
+            {
+                randomLoot = Random.Range(0, modules.Length);
+                i = 0;
+                Debug.Log(randomLoot);
+                Debug.Log("reset loop");
+            }
+            //else if(i != player.GetComponent<Inventory>().moduleGameObject.Length && player.GetComponent<Inventory>().moduleGameObject[i] != null && modules[randomLoot] != player.GetComponent<Inventory>().moduleGameObject[i])
+            //{
+            //    Debug.Log("different " + i);
+            //}
+
+
+
+
 
         }
 
