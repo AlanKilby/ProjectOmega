b0a047c615594fc592e6c23cd1cdcf0a541fe29a
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-01-20 17:28:12 +0100

    F2 Boss Teleport

---
+++ ProtoContollerRogueLike/Assets/BossTeleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTeleport : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("f2"))
+        {
+            player.transform.position = transform.position;
+        }
+    }
+}
